a4b44f7ba0f04c9b79feb0750f9538acebb79915
YoubaB05 <100047102+YoubaB05@users.noreply.github.com>
2022-02-19 21:39:24 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+from math import sqrt
+
+list = [1, 1, 2, 3, 5, 8, 13]
+num = 5**10
+
+
+def solvePoly(a, b, c):
+  delta = b**2 - 4*a*c
+  if (delta > 0):
+    x1 = (-b + sqrt(delta))/ 2*a
+    x2 = (-b - sqrt(delta))/ 2*a
+    return [x1, x2]
+  elif (delta == 0):
+    x = - b /2*a 
+    return x
+  else :
+    return "no solution In R"
+
+a = int(input("enter a\n"))
+b = int(input("enter b\n"))
+c = int(input("enter c\n"))
+
+
+solution = solvePoly(a , b , c)
+print(solution)
